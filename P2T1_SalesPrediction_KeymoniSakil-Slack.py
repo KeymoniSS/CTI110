7a43bc1bc316ac67ea1c7977558f0450b4f4523f
KeymoniSS <43156164+KeymoniSS@users.noreply.github.com>
2018-09-10 19:40:00 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_KeymoniSakil-Slack.py
@@ -0,0 +1,18 @@
+# This code displays the profit made from the projected amount of total sales
+# September 10, 2018
+# CTI-110 P2T1 - Sales Prediction
+# Keymoni Sakil-Slack
+#
+
+# Get the projected total sales
+total_sales = float(input ('Enter the projeted sales: '))
+
+# Calculate the profit as 23 percent percent of total sales
+profit = total_sales * 0.23
+
+# Display the profit
+print ('The profit is $', format(profit, ',.2f'))
+
+#Input the total sales
+#Calculate the profit as 23 percent of total sales
+#Display the profit
