ec588d9ed2f246fc2b9e8dcc2502554df735514d
KeymoniSS <43156164+KeymoniSS@users.noreply.github.com>
2018-09-10 19:43:30 -0400

    Add files via upload
    Celsius to Fahrenheit converter

---
+++ P2HW1_CelsiusConverter_KeymoniSakil-Slack.py
@@ -0,0 +1,21 @@
+# This code converts celsius to fahrenheit
+# September 10, 2018
+# CTI-110 P2HW1 - Celsius Fahrenheit Converter 
+# Keymoni Sakil-Slack
+#
+
+#define celsius value
+celsius_value = float(input('Enter the temperature in celsius: '))
+
+#formula for conversion
+fahrenheit_value = 9/5 * celsius_value + 32
+
+#print results
+print ('Your converted fahrenheit temperature:', fahrenheit_value, 'F')
+
+#prompt the user for celsius value
+#save value
+#input conversion fo fahrenheit
+#fahrenheit_value = 9/5 * celsius_value +32
+#save value
+#print fahrenheit value
