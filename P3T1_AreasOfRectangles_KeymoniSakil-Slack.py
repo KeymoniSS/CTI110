075af154871d7c57701d42579c39970f7e47c831
KeymoniSS <43156164+KeymoniSS@users.noreply.github.com>
2018-09-14 12:51:46 -0400

    Add files via upload
    This program should display the dimensions of two rectangles given which has the greater area

---
+++ P3T1_AreasOfRectangles_KeymoniSakil-Slack.py
@@ -0,0 +1,25 @@
+# This program should display of the dimesions of two rectangles given which has the greater area
+# September 14, 2018
+# CTI-110 P3T1 - AreasOfRectangles
+# Keymoni Sakil-Slack
+#
+
+#Input the length and width of rectangle 1.
+length1 = int(input('Enter the length of rectangle 1: '))
+width1 = int(input('Enter the width of rectangle 1: '))
+
+#Input the length and width of rectangle 2.
+length2 = int(input('Enter the length of rectangle 2: '))
+width2 = int(input('Enter the width of rectangle 2: '))
+
+#Calculate the area of the rectangles.
+area1 = length1 * width1
+area2 = length2 * width2
+
+#Determine which has the greater area.
+if area1 > area2:
+    print('Rectangle 1 has the greater area.')
+elif area2 > area1:
+    print('Rectangle 2 has the greater area.')
+else:
+    print('Both have the same area.')
